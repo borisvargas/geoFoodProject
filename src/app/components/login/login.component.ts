109cc10a6d4cd3c84ff2c51c5fe4f2f005d41d0f
obiguan2 <40006498+obiguan2@users.noreply.github.com>
2019-03-02 09:20:04 -0400

    agregando login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +12,59 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+       /*[ Focus input ]*/
+      /*
+      $('.input100').each(function() {
+        $(this).on('blur', function() {
+            if ($(this).val().trim() !== '') {
+                $(this).addClass('has-val');
+            } else {
+                $(this).removeClass('has-val');
+            }
+        });
+      });
+      */
+    /*==================================================================
+    [ Validate ]*/
+    /*
+    let input = $('.validate-input .input100');
+    $('.validate-form').on('submit', function() {
+        let check = true;
+        for (let i = 0; i < input.length; i++) {
+            if (validate(input[i]) === false) {
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+        return check;
+    });
+    */
+    $('.validate-form .input100').each(function() {
+        $(this).focus(function() {
+           hideValidate(this);
+        });
+    });
+    /*
+    function validate (input) {
+        if ($(input).attr('type') === 'email' || $(input).attr('name') === 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        } else {
+            if ($(input).val().trim() === '') {
+               return false;
+            }
+        }
+    }
+    */
+    function showValidate(input) {
+        let thisAlert = $(input).parent();
+        $(thisAlert).addClass('alert-validate');
+    }
+    function hideValidate(input) {
+        let thisAlert = $(input).parent();
+        $(thisAlert).removeClass('alert-validate');
+    }
   }
 
 }
