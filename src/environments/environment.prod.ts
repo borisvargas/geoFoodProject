7439728fbcadffaa129c8958afd51025cdbf8e10
obiguan2 <40006498+obiguan2@users.noreply.github.com>
2019-03-16 07:27:00 -0400

    agregando excepcion firebase para subir a AWS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  // firebase
+  firebaseConfig: {
+    apiKey: 'AIzaSyAZ3WfPp-gIrKxZYcX_K-utb35jAbMouWY',
+    authDomain: 'geofood-a5245.firebaseapp.com',
+    databaseURL: 'https://geofood-a5245.firebaseio.com',
+    projectId: 'geofood-a5245',
+    storageBucket: 'geofood-a5245.appspot.com',
+    messagingSenderId: '365205449081'
+  }
 };
